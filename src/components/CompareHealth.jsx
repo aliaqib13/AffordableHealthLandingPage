7fc2a683f104d20564ee95b3d8f8ffebe2125a48
aliaqib13 <aliaqib13@gmail.com>
2024-06-05 11:48:51 +0500

    project upload on git hub

---
+++ src/components/CompareHealth.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const CompareHealth = () => {
+  return (
+    <div className='  grid md:grid-cols-2 grid-cols-1 gap-5  py-10 w-[90%] mx-auto'>
+      <div className=' bg-[#F6F8F7]  rounded-md md:p-10 p-5 text-center w-full '>
+        <h3 className=' md:text-[30px] text-[24px] font-bold '>Compare health insurance quotes</h3>
+        <h6>
+          <span className='text-[#F37111] px-1'>NO OBLIGATION</span>TO ENROLL
+        </h6>
+        <div className=" flex md:flex-row flex-col gap-4 justify-center my-6 ">
+          <div className="flex items-center justify-between border  px-4 py-2 rounded-[10px] bg-white ">
+            <div>
+              <p className=' text-left'>Zip Code</p>
+              <input
+                className=" rounded-none border-none focus-visible:border-none"
+                type="text"
+                value={44040}
+              />
+            </div>
+            <img
+              className="absolute inset-y-0 right-0 pr-2 flex items-center mt-6 mr-2"
+              src="https://affordablehealthplans.org/wp-content/uploads/2023/04/green-check.svg"
+              alt="Success"
+              id="icon-success"
+            />
+          </div>
+          <div className=" flex justify-center items-center py-4 bg-pink-800 rounded-[10px] px-10 text-2xl font-medium text-white hover:bg-slate-500 cursor-pointer">
+            Start My Quote
+          </div>
+        </div>
+      </div>
+      <div className='bg-[#F6F8F7]  w-full rounded-md items-center justify-center gap-2 py-4  flex flex-col'>
+        <h3 className='md:text-[30px] text-[24px] font-bold'>Or give us a call</h3>
+        <h6><span className='text-[#F37111] '>THE EASIER, FASTER </span>WAY TO GET QUOTES</h6>
+        <div className=' flex flex-col gap-2 justify-center '>
+          {' '}
+          <a href="tel: 1-844-679-6803">
+            <button className=" flex justify-center items-center px-10 py-4  hover:bg-yellow-600 rounded-md bg-pink-800" type="button">
+              <span> <img className=' h10 w-10' src="https://affordablehealthplans.org/wp-content/uploads/2023/04/phone_white_200x200.gif" alt="" srcset="" /> </span>1-844-679-6803
+            </button>
+          </a>
+          <div className=' flex justify-center items-center gap-2'> <img className=' h-5 w-5' src="https://affordablehealthplans.org/wp-content/uploads/2023/04/pulse_120x120.gif" alt="" /> <p>Call for free live assistance</p></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CompareHealth
---
+++ src/components/PreferToSpeak.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const PreferToSpeak = () => {
+  return (
+    <div className=" bg-[#F6F8F7]  m-4 flex  bg-no-repeat bg-left-bottom  flex-col justify-center items-center p-6 md:py-16 md:mt-4 md:mx-32 rounded-2xl lg:bg-[url('https://affordablehealthplans.org/wp-content/uploads/2023/05/male_agent.svg')]" >
+      <div className=" flex flex-col">
+        <h2 className="  text-[35px] hidden md:block font-bold text-center">
+          Prefer to speak with an expert?
+        </h2>
+        <h2 className=" block md:hidden text-[22px] font-bold text-center">
+          To shop over the phone call
+        </h2>
+        <h6 className=" text-[22px] hidden md:block   mt-2 text-center">
+          Speak with a licensed agent and get knowledgeable advice on choosing a
+          plan that's right for you.
+        </h6>
+      </div>
+      <div className=' flex flex-col gap-5 my-10'>
+        {' '}
+        <a href="tel: 1-844-679-6803">
+          <button className=" flex justify-center items-center px-10 py-4 rounded-md bg-pink-600 hover:bg-pink-700" type="button">
+            <span> <img className=' h10 w-10' src="https://affordablehealthplans.org/wp-content/uploads/2023/04/phone_white_200x200.gif" alt="" srcset="" /> </span>1-844-679-6803
+          </button>
+        </a>
+        <div className=' flex justify-center items-center gap-2'> <img className=' h-5 w-5' src="https://affordablehealthplans.org/wp-content/uploads/2023/04/pulse_120x120.gif" alt="" /> <p>Call for free live assistance</p></div>
+      </div>
+    </div>
+  )
+}
+
+export default PreferToSpeak
